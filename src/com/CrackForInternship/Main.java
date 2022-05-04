4e60ac89c30bed102faf1257e1786f893e05252d
Nitin <nsinghboss02@gmail.com>
2022-05-04 21:10:44 +0530

    "Two Sum Problem"

--- src/com/CrackForInternship/Main.java
+++ src/com/CrackForInternship/Main.java
@@ -1,8 +1,20 @@
 package com.CrackForInternship;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            //Two sum problem
+        int[] arr = {1,4,7,2,7};
+        System.out.println(Arrays.toString(twoSum(arr,3)));
+    }
+    public static int[] twoSum(int[] arr, int target){
+        for(int i=0;i< arr.length-1;i++){
+            for(int j=i+1;j< arr.length;j++){
+                if((arr[i]+arr[j]==target)){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
